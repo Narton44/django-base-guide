66b6795b0d1619d9461778c6304ea9c18ed8dde7
Narton44 <sosokom5522@gmail.com>
2026-03-17 12:20:58 +1100

    templates - list and detail

--- shop/settings.py
+++ shop/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/' # для работы с изображениями
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')# для работы с изображениями
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'# для работы с изображениями
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')# для работы с изображениями
